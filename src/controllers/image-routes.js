e6245978b964c258fa583e40d10469788200c941
Burak <14030652+BurakAy@users.noreply.github.com>
2024-10-22 20:47:42 -0700

    implementing multer middleware

--- src/controllers/image-routes.js
+++ src/controllers/image-routes.js
@@ -1,12 +1,14 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const { imageUpload } = require("../service/image-service.js");
 const { authenticateToken } = require("../util/authentication.js");
 const { logger } = require("../util/logger.js");
+const upload = multer();
 
-router.post("/", authenticateToken, async (req, res) => {
+router.post("/", upload.single("file"), authenticateToken, async (req, res) => {
   try {
-    const response = await imageUpload(req.body);
+    const response = await imageUpload(req.file);
     res.status(200);
     res.send(response);
   } catch (err) {
