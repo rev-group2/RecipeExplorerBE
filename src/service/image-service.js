1f76b8842a44a1ef865a77ebee0ac97cde6c5e58
Burak <burakadmr@gmail.com>
2024-10-24 06:15:39 -0700

    Image upload (#10)
    * Installed aws client-s3 and s3-request-presigner used for uploading images and getting url
    
    * Route for image upload
    
    * Route for image upload
    
    * commands for putting and getting image and url
    
    * handling image upload request
    
    * Added multer package to handle multipart formdata in request for image upload
    
    * implementing multer middleware
    
    * fixed imports
    
    * passing file attributes as arguments for upload and presigned url
    
    * removed encoding
    
    * Added upload file size limit
    
    * Tweaked error messages
    
    * limiting image file size to 3mb
    
    * updated client-secrets-manager
    
    * changed bucket name

---
+++ src/service/image-service.js
@@ -0,0 +1,26 @@
+const {
+  uploadImageToBucket,
+  getPreSignedUrl
+} = require("../repository/image-dao");
+
+async function imageUpload(imageFile) {
+  try {
+    if (!imageFile) {
+      throw new Error("Missing image file");
+    }
+
+    const bucket = "recipe-explorer";
+    const objectKey = `images/${Date.now()}_${imageFile.originalname}`;
+    const contentType = imageFile.mimetype;
+
+    await uploadImageToBucket(bucket, objectKey, contentType, imageFile.buffer);
+
+    const presignedUrl = await getPreSignedUrl(bucket, objectKey);
+
+    return presignedUrl;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+}
+
+module.exports = { imageUpload };
